908367f3a819937dc50109542a8513e6864a1218
stepantishhen <stepantishhen@mail.ru>
2023-05-17 17:24:12 +0300

    Edit settings.py

--- grademaster/settings.py
+++ grademaster/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'grademaster.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 # using sqlite3 database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+		'default': {
+      	'ENGINE': 'django.db.backends.postgresql',
+      	'HOST' : os.environ.get('POSTGRES_HOST', 'localhost'),
+      	'NAME': os.environ.get('POSTGRES_DB', 'db_name'),
+      	'USER': os.environ.get('POSTGRES_USER', 'username'),
+      	'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
+      	'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 # DATABASES = {
